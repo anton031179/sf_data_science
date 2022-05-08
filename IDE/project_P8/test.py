da14a5884df564093249519d2fa0e9a3e4884de6
anton031179 <anton_suhoplyuev@mail.ru>
2022-05-08 23:52:23 +0500

    fixation_0805

---
+++ IDE/project_P8/test.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+#for i in range(5):
+  
+    # Any number can be used in place of '0'.
+#np.random.seed(1)
+  
+    # Generated random number will be between 1 to 1000.
+    #print(np.random.randint(1, 1000)) 
+random_array = np.random.randint(1, 1000, size=(1000))
+#for number in random_array:
+print(random_array)

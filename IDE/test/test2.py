d5db4e9e248301e66f2116f7bdac0447eaf21a3a
anton031179 <anton_suhoplyuev@mail.ru>
2022-06-13 21:19:54 +0500

    fix_130622

--- IDE/test/test2.py
+++ IDE/test/test2.py
@@ -1,27 +1,38 @@
+# Выполните все действия, указанные в комментариях в файле
 import numpy as np
+mystery = np.array([12279., -26024., 28745., nan, 31244., -2365., -6974., -9212., nan, -17722., 16132., 25933., nan, -16431., 29810.])
+print (mystery)
+# Получите булевый массив с информацией о np.nan в массиве mystery
+# True - значение пропущено, False - значение не пропущено
+nans_index = np.isnan(mystery)
+print(nans_index)
+# В переменную n_nan сохраните число пропущенных значений
+n_nan = 3
 
-#def predict8(number: int = 1) -> int:
+# Заполните пропущенные значения в массиве mystery нулями
+mystery[np.isnan(mystery)] = 0
+print(mystery)
 
-number = np.random.randint(1, 1000) # загадываем рандомное число, используя генератор рандомных чисел
-print(number)
-count = 0
-mn = 1
-mx = 1000
-    
-while True:
-    count += 1
-        #print(count)
-    md = round((mn + mx)/2)
-        
-    if md > number:
-        mx = md
-    elif md < number:
-        mn = md
-    else:
-        print(f'Компьютер угадал загаданное число за {count} попыток. Это число {number}')
-        break # конец игры и выход из цикла
-     
-    #print(count, number) 
-    #return count
+# Поменяйте тип данных в массиве mystery на int32
+mystery=np.int32(mystery)
+print(mystery)
+
+# Отсортируйте значения в массиве по возрастанию и сохраните
+# результат в переменную array
+array = np.sort(mystery)
+
+# Сохраните в массив table двухмерный массив, полученный из массива array
+# В нём должно быть 5 строк и 3 столбца. Причём порядок заполнения должен быть
+# по столбцам! Например, 1, 2, 3, 4 -> 1    3
+#                                      2    4
+table = array.reshape((5, 3), order='F')
+print(table)
+#  Сохраните в переменную col средний столбец из table
+col = table[:,1]
+print(col)
+
+
+
+
     
 
--- README.md
+++
@@ -1,7 +0,0 @@
-# My data science projects
-From the [Skillfactory Data Science course](https://skillfactory.ru/ data-scientist)
-
-## Проекты
-[Проект 0. Игра: Угадай число](https://github.com/SkilfactoryDS/ sf_data_science/tree/main/project_0)
-* [Проект 1. 	] (	)
-* [Проект 2. 	] (	)
